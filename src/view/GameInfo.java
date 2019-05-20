dfe74221d0868016775d6e5e0d5a63dc9571a352
DragonHee <50479743+DragonHee@users.noreply.github.com>
2019-05-20 20:54:57 +0900

    포커스 클래스 분리

--- src/view/GameInfo.java
+++ src/view/GameInfo.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -16,16 +14,10 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import etc.ChangePanelService;
-import view.MainView.Handler;
 
 public class GameInfo extends JPanel {
    public GameInfo() {
-	   this.addComponentListener( new ComponentAdapter() {
-	        @Override
-	        public void componentShown( ComponentEvent e ) {
-	        	 GameInfo.this.requestFocusInWindow();
-	        }
-	    });
+	   this.addComponentListener(new FocusHandler());
 	   this.addKeyListener(new Handler());
       Color bg=new Color(197, 90, 17);
       Font font=new Font("nanum brush script", Font.BOLD, 45);
