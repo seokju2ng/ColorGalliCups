f1066c343070bff7255abba9bba3c71e7f43eeba
DragonHee <50479743+DragonHee@users.noreply.github.com>
2019-05-20 21:23:48 +0900

    포커스 클래스 분리

--- src/view/WaitingRoomNormal.java
+++ src/view/WaitingRoomNormal.java
@@ -5,10 +5,6 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.Random;
 
 import javax.swing.ImageIcon;
@@ -28,12 +24,7 @@ public class WaitingRoomNormal extends JPanel{
 	private JButton backButton;
 	
 	public WaitingRoomNormal() {
-		this.addComponentListener( new ComponentAdapter() {
-	        @Override
-	        public void componentShown( ComponentEvent e ) {
-	        	 WaitingRoomNormal.this.requestFocusInWindow();
-	        }
-	    });
+		this.addComponentListener(new FocusHandler());
 		makeUI();
 	}
 	private void makeUI() {
