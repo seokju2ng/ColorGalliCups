0fcb11678bc7bcf7d6779016670bc90c8e65b051
DragonHee <50479743+DragonHee@users.noreply.github.com>
2019-05-20 10:52:20 +0900

    화면전환시 focus수정

--- src/view/WaitingRoomNormal.java
+++ src/view/WaitingRoomNormal.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Random;
@@ -26,6 +28,12 @@ public class WaitingRoomNormal extends JPanel{
 	private JButton backButton;
 	
 	public WaitingRoomNormal() {
+		this.addComponentListener( new ComponentAdapter() {
+	        @Override
+	        public void componentShown( ComponentEvent e ) {
+	        	 WaitingRoomNormal.this.requestFocusInWindow();
+	        }
+	    });
 		makeUI();
 	}
 	private void makeUI() {
