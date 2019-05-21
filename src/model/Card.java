c79736d24bed922611ba3ea8574392026bca143b
seokju2ng <43347250+seokju2ng@users.noreply.github.com>
2019-05-21 19:11:53 +0900

    model card entity

---
+++ src/model/Card.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Card {
+	private int num;
+	private String path;
+	private String answer;
+	
+	public Card() {}
+	public Card(String path, String answer) {
+		this.path = path; this.answer = answer;
+	}
+	public Card(int num, String path, String answer) {
+		this.num = num; this.path = path; this.answer = answer;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public String toString() {
+		return "Card[card_num:"+num+"/img_path:"+path+"/answer:"+answer+"]";
+	}
+}
