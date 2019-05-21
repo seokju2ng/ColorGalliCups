2887e2a0bb4f4187ac1f965d1991e8d239496f56
chlalstjd430 <50758600+chlalstjd430@users.noreply.github.com>
2019-05-21 21:30:56 +0900

    Add files via upload
    Cups Class this is  used to attatch Board Class.

---
+++ src/etc/Cups.java
@@ -0,0 +1,55 @@
+package etc;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Cups {
+	//ColorCups
+	private JLabel cups[][][];
+	//imgType
+	private String imgType;
+	int cupCnt;
+	int panelCnt;
+	//dual : DualPlayMode와 NetworkPlayMode에서 사용할 이미지 경로
+	//solo : SoloPlayMode에서 사용할 이미지 경로
+	public Cups(String imgType,int cupCnt, int panelCnt) {
+		this.cupCnt = cupCnt;
+		this.panelCnt = panelCnt;
+		this.setImgType(imgType);
+		cups = new JLabel[cupCnt][panelCnt][panelCnt];
+		for(int i = 0; i < panelCnt; i++) {
+			for(int j = 0; j < panelCnt; j++) {
+				for(int k= 0; k < cupCnt; k++) {
+					cups[k][i][j] = new JLabel();
+				}
+			}
+		}
+	}
+	public void setImg(int cupIndex, String imgPath) {
+		for(int i = 0; i < panelCnt; i++) {
+			for(int j = 0; j < panelCnt; j++) {
+				cups[cupIndex][i][j].setIcon(new ImageIcon(imgPath));
+			}
+		}
+	}
+	public void setAllBounds(int x, int y, int area, int height, int interval) {
+		for(int i = 0; i < panelCnt; i++) {
+			for(int j = 0; j < panelCnt; j++) {
+				for(int k= 0; k < cupCnt; k++) {
+					cups[k][i][j].setBounds(x, y + j * interval, area, height);
+					cups[k][i][j].setVisible(false);
+				}
+			}
+		}		
+	}
+	public JLabel getCups(int colorIndex, int col, int row) {
+		return cups[colorIndex][col][row];
+	}
+	public String getImgType() {
+		return imgType;
+	}
+	public void setImgType(String imgType) {
+		this.imgType = imgType;
+	}
+}
