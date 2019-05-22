541080b8887dae72718f8cfbe099c78cddaf1c4e
chlalstjd430 <50758600+chlalstjd430@users.noreply.github.com>
2019-05-22 13:53:49 +0900

    Update KeyUpDownHandler.java
    edit int to MyIndex Class

--- src/view/KeyUpDownHandler.java
+++ src/view/KeyUpDownHandler.java
@@ -7,38 +7,39 @@ import javax.swing.JLabel;
 
 public class KeyUpDownHandler extends KeyAdapter
 {
-	private Integer cor;
+	private MyIndex cor;
 	private JLabel leftCursorArr[];
 	private JLabel rightCursorArr[];
 	private int num;
-	public KeyUpDownHandler(Integer cor, int num,  JLabel[] leftCursorArr, JLabel[] rightCursorArr) {
+	public KeyUpDownHandler(MyIndex cor, int num,  JLabel[] leftCursorArr, JLabel[] rightCursorArr) {
 		this.cor = cor;
 		this.num = num;
 		this.leftCursorArr = leftCursorArr;
-		this.rightCursorArr = rightCursorArr;
-
-		System.out.println("keyupdown"+leftCursorArr);
+		this.rightCursorArr = rightCursorArr; 
+		
 	}
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			System.out.println("down");
-			if (cor != num) {
-				leftCursorArr[cor].setVisible(false);
-				rightCursorArr[cor].setVisible(false);
-				cor = cor + 1;
-				leftCursorArr[cor].setVisible(true);
-				rightCursorArr[cor].setVisible(true);
+			
+			if (cor.getIndex() != num) {
+				leftCursorArr[cor.getIndex()].setVisible(false);
+				rightCursorArr[cor.getIndex()].setVisible(false);
+				cor.plus();
+				leftCursorArr[cor.getIndex()].setVisible(true);
+				rightCursorArr[cor.getIndex()].setVisible(true);
 			}
+			System.out.println("down"+cor);
 		} else if (e.getKeyCode() == KeyEvent.VK_UP) {
 			System.out.println("up");
-			if (cor != 0) {
-				leftCursorArr[cor].setVisible(false);
-				rightCursorArr[cor].setVisible(false);
-				cor = cor - 1;
-				leftCursorArr[cor].setVisible(true);
-				rightCursorArr[cor].setVisible(true);
+			if (cor.getIndex() != 0) {
+				leftCursorArr[cor.getIndex()].setVisible(false);
+				rightCursorArr[cor.getIndex()].setVisible(false);
+				cor.minus();
+				leftCursorArr[cor.getIndex()].setVisible(true);
+				rightCursorArr[cor.getIndex()].setVisible(true);
 			}
+			System.out.println("up" + cor);
 		} 
 	}
 
